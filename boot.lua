873ab62460d692b13684de5564b8518b199cebc4
dayflare <145824934+dayflaree@users.noreply.github.com>
2025-07-12 20:52:02 -0600

    Create boot.lua

---
+++ boot.lua
@@ -0,0 +1,9 @@
+local MODULE = MODULE
+
+MODULE.Name = "Monolith UI"
+MODULE.Ddescription = "Replaces all the Parallax Derma with Monolith styled UI."
+MODULE.Author = "Riggs"
+
+function MODULE:Initialize()
+    ax.util:Print("I'm gay for Monolith UI!")
+end
